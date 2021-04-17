4852a7e5712eafb23287b462333af4b4ac3d38a4
Mergen Narminov <mergen.narminov@visitech.ru>
2021-04-17 18:42:38 +0300

    fix: modal

--- front/src/pages/staff/Staff.js
+++ front/src/pages/staff/Staff.js
@@ -1,51 +1,17 @@
 import React, { useEffect, useState } from 'react';
-import { Row, Col, Table, Modal } from 'reactstrap';
+import { Row, Col, Table, Modal, FormGroup, Label, Input, InputGroup, Button, InputGroupAddon, InputGroupText } from 'reactstrap';
 import ModalBody from 'reactstrap/lib/ModalBody';
 import Widget from '../../components/Widget';
 import s from '../tables/static/Static.module.scss';
 
 const thead = ['#', 'ФИО', 'Телефон', 'Email', 'Дата рождения', 'Среднее профессиональное образование']
-const users = [
-  {
-    id: 1,
-    fio: 'Достоевский Федор Михайлович',
-    phone: '9992221155',
-    email: 'sam@example.com',
-    DateBirth: "25.06.1994",
-    education: "Асиновский техникум промышленной индустрии и сервиса"
-  },
-  {
-    id: 2,
-    fio: 'Раскольников Родион Романович',
-    phone: '9992221155',
-    email: 'fat.thor@example.com',
-    DateBirth: "25.06.1994",
-    education: "Каргасокский техникум промышленности и речного транспорта"
-  },
-  {
-    id: 3,
-    fio: 'Мармеладова Соня Семеновна',
-    phone: '9992221155',
-    email: 'larry@example.com',
-    DateBirth: "25.06.1994",
-    education: "Томский аграрный колледж"
-  },
-  {
-    id: 4,
-    fio: 'Раскольникова Авдотья Романовна',
-    phone: '9992221155',
-    email: 'peter@example.com',
-    DateBirth: "25.06.1994",
-    education: "Кожевниковский техникум агробизнеса"
-  }
-]
 
 const Staff = () => {
   const [statements, setStatements] = useState([])
   const [isOpen, setIsOpen] = useState(false)
-  const [activeNomber, setActiveNomber] = useState(users[0])
+  const [activeNumber, setActiveNumber] = useState({})
 
-  const change = (e) => setActiveNomber(users[e - 1])
+  const change = (e) => setActiveNumber(statements[e])
   const toggle = () => setIsOpen(!isOpen)
 
   useEffect(() => {
@@ -59,10 +25,89 @@ const Staff = () => {
     }
   }, [])
 
-
-
   return (
     <div className={s.root}>
+      <Widget title={<h5>Поиск заявлений</h5>}>
+        <Row>
+          <Col sm={4} lg={4}>
+            <FormGroup>
+              <Label>Введите ФИО</Label>
+              <InputGroup className='input-group-no-border'>
+                <InputGroupAddon addonType='prepend'>
+                  <InputGroupText>
+                    <i className='fa fa-search text-white' />
+                  </InputGroupText>
+                </InputGroupAddon>
+                <Input
+                  id='search-input'
+                  className='input-transparent'
+                  placeholder='Найти'
+                />
+              </InputGroup>
+            </FormGroup>
+          </Col>
+          <Col sm={4} lg={4}>
+            <FormGroup>
+              <Label for="exampleSelect">Среднее профессиональное образование</Label>
+              <Input type="select" name="select1" id="exampleSelect">
+                <option>Асиновский техникум промышленной индустрии и сервиса</option>
+                <option>Каргасокский техникум промышленности и речного транспорта</option>
+                <option>Кожевниковский техникум агробизнеса</option>
+                <option>Колледж индустрии питания, торговли и сферы услуг</option>
+                <option>Томский аграрный колледж</option>
+              </Input>
+            </FormGroup>
+          </Col>
+          <Col sm={2} lg={2}>
+            <FormGroup>
+              <Label for="exampleSelect">Многодентная семья</Label>
+              <Input type="select" name="select1" id="exampleSelect">
+                <option>Нет</option>
+                <option>Да</option>
+              </Input>
+            </FormGroup>
+          </Col>
+        </Row>
+        <Row>
+          <Col sm={3} lg={3}>
+            <FormGroup>
+              <Label for="exampleSelect">Получают категорию "B"</Label>
+              <Input type="select" name="select1" id="exampleSelect">
+                <option>Нет</option>
+                <option>Да</option>
+              </Input>
+            </FormGroup>
+          </Col>
+          <Col sm={3} lg={3}>
+            <FormGroup>
+              <Label for="exampleSelect">С инвалидностью</Label>
+              <Input type="select" name="select1" id="exampleSelect">
+                <option>Нет</option>
+                <option>Да</option>
+              </Input>
+            </FormGroup>
+          </Col>
+          <Col sm={3} lg={3}>
+            <FormGroup>
+              <Label for="exampleSelect">Сироты</Label>
+              <Input type="select" name="select1" id="exampleSelect">
+                <option>Нет</option>
+                <option>Да</option>
+              </Input>
+            </FormGroup>
+          </Col>
+          <Col sm={3} lg={3}>
+            <FormGroup>
+              <Label for="exampleSelect">Семья</Label>
+              <Input type="select" name="select1" id="exampleSelect">
+                <option>Полная</option>
+                <option>Неполная</option>
+              </Input>
+            </FormGroup>
+          </Col>
+        </Row>
+        <Button style={{ background: 'rgba(0,0,0,0.6)', float: 'right' }}>Фильтровать</Button>
+      </Widget>
       <h2 className='page-title'>Список подавших заявление</h2>
       <Row>
         <Col lg={12}>
@@ -76,8 +121,8 @@ const Staff = () => {
                 </thead>
                 {/* eslint-disable */}
                 <tbody>
-                  {statements.map(user =>
-                    <tr key={user.id} onClick={() => { change(user.id); toggle(); }}>
+                  {statements.map((user, index) =>
+                    <tr key={user.id} onClick={() => { change(index); toggle(); }}>
                       <td>{user.id}</td>
                       <td>{user.fio}</td>
                       <td>{user.phone}</td>
@@ -91,44 +136,43 @@ const Staff = () => {
                 </tbody>
                 {/* eslint-enable */}
               </Table>
-
-              {
-                <Modal isOpen={isOpen} toggle={toggle} style={{ maxWidth: "1100px" }}>
-                  <ModalBody>
-                    <Row>
-                      {/* <Col xs={12} md={1}>
-                        <img src="/static/media/a5.jpg" style={{ width: "170%", height: "auto", objectFit: "contain" }}></img>
-                      </Col> */}
-                      <Col xs={12} md={12}>
-                        <Table className='table-hover'>
-                          <thead>
+              <Modal isOpen={isOpen} toggle={toggle} style={{ maxWidth: "1100px" }}>
+                <ModalBody>
+                  <Row>
+                    {/* <Col xs={12} md={1}>
+                      <img src="/static/media/a5.jpg" style={{ width: "170%", height: "auto", objectFit: "contain" }}></img>
+                    </Col> */}
+                    <Col xs={12} md={12}>
+                      <Table className='table-hover'>
+                        <thead>
+                          <tr>
+                            {thead.map((el, index) => <th key={index}>{el}</th>)}
+                            <th>Специализация</th>
+                            {/* <th>Домашний телефон</th>
+                            <th>Наименование школы</th>
+                            <th>Номер аттестата</th>
+                            <th>Форма обучения</th> */}
+                          </tr>
+                        </thead>
+                        <tbody>
+                          {activeNumber &&
                             <tr>
-                              {thead.map((el, index) => <th key={index}>{el}</th>)}
-                              <th>Специализация</th>
-                              {/* <th>Домашний телефон</th>
-                              <th>Наименование школы</th>
-                              <th>Номер аттестата</th>
-                              <th>Форма обучения</th> */}
-                            </tr>
-                          </thead>
-                          <tbody>
-                            <tr>
-                              <td>{activeNomber.id}</td>
-                              <td>{activeNomber.fio}</td>
-                              <td>{activeNomber.phone}</td>
+                              <td>{activeNumber.id}</td>
+                              <td>{activeNumber.fio}</td>
+                              <td>{activeNumber.phone}</td>
                               <td>
-                                <a href='#'>{activeNomber.email}</a>
+                                <a href='#'>{activeNumber.email}</a>
                               </td>
-                              <td>{activeNomber.birthday}</td>
-                              <td>{activeNomber.education_form}</td>
+                              <td>{activeNumber.birthday}</td>
+                              <td>{activeNumber.education_form}</td>
                             </tr>
-                          </tbody>
-                        </Table>
-                      </Col>
-                    </Row>
-                  </ModalBody>
-                </Modal>
-              }
+                          }
+                        </tbody>
+                      </Table>
+                    </Col>
+                  </Row>
+                </ModalBody>
+              </Modal>
             </div>
           </Widget>
         </Col>
